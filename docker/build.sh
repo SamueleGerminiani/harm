aac8de3486d51df7093c4731632e565025af1c8b
Samuele Germiniani <samuele.germiniani@univr.it>
2022-07-25 14:09:54 +0100

    added target platform in Dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,1 @@
- docker build --build-arg CACHE_DATE=$(date +%Y-%m-%d:%H:%M:%S) -t samger/harm .
+ docker build --platform linux/amd64 --build-arg CACHE_DATE=$(date +%Y-%m-%d:%H:%M:%S) -t samger/harm .
